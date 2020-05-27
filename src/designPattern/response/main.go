324ba2e14dd9e7003c85eefb1a9eae2dc4d24d49
lizhi <1458705589@qq.com>
2020-05-27 10:17:01 +0800

    golint

--- src/designPattern/response/main.go
+++ src/designPattern/response/main.go
@@ -8,17 +8,17 @@ import (
 // 拆分程序：拆分为不同小功能，可以自己控制和替换为不同实现
 func main() {
 
-	resposeChain := make(map[string]utils.ResponseInter, 1)
+	responseChain := make(map[string]utils.ResponseInter, 1)
 
-	resposeChain["loginCheck"] = utils.LoginCheck{}
-	resposeChain["nameLenCheck"] = utils.NameLenCheck{}
+	responseChain["loginCheck"] = utils.LoginCheck{}
+	responseChain["nameLenCheck"] = utils.NameLenCheck{}
 
 	request := utils.Request{
 		Name:        "lizhi",
 		LoginStatus: false,
 	}
 
-	for k, v := range resposeChain {
+	for k, v := range responseChain {
 		fmt.Println(k, " checking ......")
 		v.Check(&request)
 		fmt.Println(k, " check success !!!")
