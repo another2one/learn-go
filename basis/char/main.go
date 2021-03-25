91cdf778758c0d5bb95f881acf2c33d828c19d7f
lizhi <1458705589@qq.com>
2021-03-25 17:59:15 +0800

    char

--- basis/char/main.go
+++ basis/char/main.go
@@ -5,12 +5,17 @@ import (
 	"unsafe"
 )
 
-func main()  {
+func main() {
 
-	var c1 byte = 'c';
-	var c2 rune = '你';
+	// var c1 byte = 'c'
+	// var c2 rune = '你' // utf-8 占用3字节，无法用byte表示
+	c1 := 'c'
+	c2 := '你'
+	c3 := []byte("cc")
+	c4 := []byte("c1你")
 
-	fmt.Printf("c1 type is %T, size is %d, c1 type is %T, c2 size is %d \n", c1, unsafe.Sizeof(c1), c2, unsafe.Sizeof(c2));
-	fmt.Println("c1 =", c1, "\t c2 =", c2);
-	fmt.Printf("c1 = %c \t c2 = %c \n", c1, c2);
+	fmt.Printf("c1 type is %T, size is %d, c2 type is %T, size is %d, c3 type is %T, size is %d, c4 type is %T, size is %d \n",
+		c1, unsafe.Sizeof(c1), c2, unsafe.Sizeof(c2), c3, len(c3), c4, len(c4))
+	fmt.Println("c1 =", c1, "\t c2 =", c2)
+	fmt.Printf("c1 = %c \t c2 = %c \n", c1, c2)
 }
--- test.go
+++ test.go
@@ -3,11 +3,41 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"time"
 )
 
+var v = 4
+
 func main() {
 
+	// dialSocksProxy, err := proxy.SOCKS5("tcp", "127.0.0.1:10808", nil, proxy.Direct)
+	// if err != nil {
+	// 	fmt.Println("Error connecting to proxy:", err)
+	// }
+	// tr := &http.Transport{Dial: dialSocksProxy.Dial}
+
+	// // Create client
+	// myClient := &http.Client{
+	// 	Transport: tr,
+	// }
+	// response, err := myClient.Get("https://etherscan.io/")
+	// if err != nil {
+	// 	fmt.Println("Error get url:", err)
+	// }
+	// defer response.Body.Close()
+	// var by []byte
+	// by, _ = ioutil.ReadAll(response.Body)
+	err := writeFile("et.html", []byte("我是666\n"))
+	fmt.Println(err)
+	v := 1 //声明
+	if v == 1 {
+		v = 3 // 赋值
+		fmt.Println(v)
+	}
+	fmt.Println(v) //依然可以访问
+	return
 	ch := 'b'
 	fmt.Println(ch / 2.0)
 
@@ -32,4 +62,18 @@ func main() {
 
 func printAddress(v interface{}) {
 	fmt.Printf("v address is %p \n", &v)
+}
+
+// writeFile 实现追加写入
+func writeFile(filename string, data []byte) error {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE, 0666)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	n, err := file.Write(data)
+	if err != nil && n < len(data) {
+		err = io.ErrShortWrite
+	}
+	return err
 }
