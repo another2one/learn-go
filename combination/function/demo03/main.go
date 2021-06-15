f296e16fdf2e99938c0c8a965ef1f7b8bc6fdeec
lizhi <1458705589@qq.com>
2021-06-15 16:10:21 +0800

    merge

---
+++ combination/function/demo03/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// TODO: 汇编查看——到底是啥
+func main() {
+	s, q := a()
+	fmt.Println(s, q)
+
+	ss, _ := a()
+	fmt.Println(ss)
+
+	fmt.Println(a())
+	fmt.Println(a1(10, 20))
+}
+
+// ??? _难道只是一个特殊变量
+func a() (int, _ int) {
+	_ = 66
+	return 1, 66
+}
+
+// ???
+func a1(i, _ int) int {
+	_ = 66
+	return i
+}
